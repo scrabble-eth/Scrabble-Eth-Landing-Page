8854609ef8ddc37badc808ca43f0698045f65720
Geetansh Atrey <atrey.geetansh@gmail.com>
2021-04-22 23:29:28 +0530

    Build Added.

---
+++ build/precache-manifest.53e4b6eecf60ea1f7e465e4b853bc61f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1aa645e92601eda75a4968fa6eb1dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4c269030d9bb5331aa8",
+    "url": "/static/css/2.724cf544.chunk.css"
+  },
+  {
+    "revision": "47586f8981bcef4e97af",
+    "url": "/static/css/main.d4fba6cb.chunk.css"
+  },
+  {
+    "revision": "d4c269030d9bb5331aa8",
+    "url": "/static/js/2.05a23de0.chunk.js"
+  },
+  {
+    "revision": "aa94962c6278bf8f6df236d7f39cf3dc",
+    "url": "/static/js/2.05a23de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47586f8981bcef4e97af",
+    "url": "/static/js/main.c13ba378.chunk.js"
+  },
+  {
+    "revision": "73237cc1df829a1706ee",
+    "url": "/static/js/runtime-main.9f870c75.js"
+  },
+  {
+    "revision": "90dbadf75973056727c07a3361d01311",
+    "url": "/static/media/company_team.90dbadf7.jpg"
+  },
+  {
+    "revision": "f93e69d7d04e6b46b527164be596fe67",
+    "url": "/static/media/fun.f93e69d7.png"
+  },
+  {
+    "revision": "5f877ea3e46ce69a64c096539b165083",
+    "url": "/static/media/future.5f877ea3.png"
+  },
+  {
+    "revision": "fa0148fddb91dd5919440e4d4b8c22f1",
+    "url": "/static/media/futureplans.fa0148fd.gif"
+  },
+  {
+    "revision": "3422544f88e0e7d94e3bbc6d344a4f8e",
+    "url": "/static/media/gamelogic.3422544f.gif"
+  },
+  {
+    "revision": "1d6c681d21b01c59cb8561d48dcd2e6e",
+    "url": "/static/media/idea.1d6c681d.gif"
+  },
+  {
+    "revision": "40e82a0b73f6a88db42292d794dbc3b9",
+    "url": "/static/media/idea.40e82a0b.png"
+  },
+  {
+    "revision": "187d484fd8701efa19da26b5f42f9884",
+    "url": "/static/media/insidebox.187d484f.gif"
+  },
+  {
+    "revision": "4c167298788df53b54369d7ce92a5100",
+    "url": "/static/media/logo.4c167298.gif"
+  },
+  {
+    "revision": "018103c5a778bb73b6102b9c3fe9860e",
+    "url": "/static/media/logo_big.018103c5.png"
+  },
+  {
+    "revision": "9d674b827fcd5f006b7ecabbc6961875",
+    "url": "/static/media/overview.9d674b82.gif"
+  },
+  {
+    "revision": "ba4b2041249b010402e6eae46460975f",
+    "url": "/static/media/permission.ba4b2041.png"
+  },
+  {
+    "revision": "399a197399b56f7b83c510227db17348",
+    "url": "/static/media/rewards.399a1973.gif"
+  },
+  {
+    "revision": "f871f503321b6a7bf57874a4c1bb10d3",
+    "url": "/static/media/subtitle.f871f503.png"
+  }
+]);
